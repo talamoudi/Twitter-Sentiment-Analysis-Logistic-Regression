b270e1f41ce95ae3938f08013188f92aa37db7bc
Tamer Alamoudi <alamoudi@cs.fsu.edu>
2021-03-11 14:57:19 -0500

    refactored

--- train_logistic_model.py
+++ train_logistic_model.py
@@ -20,6 +20,6 @@ if __name__ == "__main__":
     W, Losses, Accuracies = train_logistic_model(X, Y, lr = 1e-8, epochs = 1300, verbose = True)
     plot_training(Losses, Accuracies)
     
-    test_logistic_model(Xtest,Ytest,W)
+    test_logistic_model(Xtest, Ytest, W)
 
     save_model("./Results/twitter_sentiment.model", model = (W, frequencies))
